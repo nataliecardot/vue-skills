cfcdcd04edea9ade0c4b8b719bf84aabaf763764
Natalie-Cardot <nataliecardot@gmail.com>
2020-01-14 14:01:25 -0800

    Add styling

---
+++ src/components/Skills.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in skills" :key="index">{{ data.skill }}</li>
+      </ul>
+
+      <p>These are the skills that you possess.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+  data() {
+    return {
+      skills: [{ skill: "Vue.js" }, { skill: "React" }],
+      bgColor: "yellow",
+      bgWidth: "100%",
+      bgHeight: "30px"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.holder {
+  background: #fff;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  border-left: 5px solid #3eb3f6;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
+
+p {
+  text-align: center;
+  padding: 30px 0;
+  color: gray;
+}
+
+.container {
+  box-shadow: 0px 0px 40px lightgray;
+}
+</style>
