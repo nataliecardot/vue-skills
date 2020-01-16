61f77b84db15a239caeaeca0c26fd49e26151f81
Natalie-Cardot <nataliecardot@gmail.com>
2020-01-16 15:46:27 -0800

    Add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
-    <Skills />
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <!-- Have to use this wherever you want router to display template -->
+    <router-view />
   </div>
 </template>
 
 <script>
-import Skills from "./components/Skills.vue";
+import Skills from './components/Skills.vue';
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
     Skills
   }
@@ -16,14 +22,19 @@ export default {
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+* {
+  box-sizing: border-box;
+}
+
 body {
   background-color: #eee;
-  font-family: "Montserrat", sans-serif;
+  font-family: 'Montserrat', sans-serif;
   /* display; flex along with setting auto margin for flex item causes it to be centered */
-  display: flex;
-  margin: 0;
-  padding: 2rem;
+  display: grid;
+  justify-items: center;
+  padding-top: 50px;
 }
 
 body,
@@ -36,7 +47,16 @@ html {
   margin: auto;
 }
 
-* {
-  box-sizing: border-box;
+nav {
+  padding: 20px 20px 20px 0;
+}
+
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  margin-right: 15px;
 }
 </style>
---
+++ src/components/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>Bla bla bla</p>
+  </div>
+</template>
